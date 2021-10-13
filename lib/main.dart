4a50e91bc4e221a71ced9d7a09dc30a99bcfb122
jugjug7531 <24523617+jugjug7531@users.noreply.github.com>
2021-10-13 09:27:23 +0900

    問題ページの基底クラス作成 画面遷移のルーティング設定追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flip_card/flip_card.dart';
 
+import 'quiz/quiz1.dart';
+import 'quiz/quiz2.dart';
+
+
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  final String title = 'ババ抜きサドンデスクイズ';
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ババ抜きサドンデスクイズ',
+      title: title,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'ババ抜きサドンデスクイズ'),
+      // デフォルトのルーティング
+      initialRoute: '/',
+      // ルーティングの一覧を設定
+      routes: {
+        '/': (context) => MyHomePage(title : title),
+        '/1': (context) => Quiz1(),
+        '/2': (context) => Quiz2(),
+      },
     );
   }
 }
@@ -31,107 +46,20 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  // 選択肢カード
-  List<Map<String, String>> choices = [
-    {
-      "choice": "夜に駆ける",
-      "answer": "true"
-    },
-    {
-      "choice": "おっぱい",
-      "answer": "true"
-    },
-    {
-      "choice": "ナンプラー\n日和",
-      "answer": "true"
-    },
-    {
-      "choice": "1987→",
-      "answer": "true"
-    },
-    {
-      "choice": "孫悟空",
-      "answer": "true"
-    },
-    {
-      "choice": "トンボ\n飛べなかった",
-      "answer": "false"
-    }
-  ];
-
-  // カードを並べた時の全体の横幅
-  double _cardInterval = 200;
-  // カードの横幅
-  double _cardWidth = 150;
-  // カードの縦幅
-  double _cardHeight = 200;
-
-
-  // 選択肢カードWidget作成
-  Widget createChoiceWidget(String? choice, String? answer){
-    if(choice == null){
-      print("[ERROR] choice is null");
-      choice = 'null';
-    }
-    if(answer == null){
-      print("[ERROR] answer is null");
-      answer = 'true';
-    }
-    return SizedBox(
-      width: _cardWidth,
-      height: _cardHeight,
-      child:Card(
-        elevation: 0.0,
-        margin: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0, bottom: 0.0),
-        color: Color(0x00000000),
-        child: FlipCard(
-          direction: FlipDirection.HORIZONTAL,
-          speed: 1000,
-          onFlipDone: (status) {
-            print(status);
-          },
-          front: Container(
-            decoration: BoxDecoration(
-              color: Color(0x000000),
-              border: Border.all(color: Colors.black),
-              borderRadius: BorderRadius.all(Radius.circular(8.0)),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  choice,  //選択肢
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                )
-              ],
-            ),
-          ),
-          back: Container(
-            decoration: BoxDecoration(
-              color: Color(0x000000),
-              border: Border.all(color: Colors.black),
-              borderRadius: BorderRadius.all(Radius.circular(8.0)),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  answer,  //答え
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                )
-              ],
-            ),
-          ),
+  /* 問題選択ボタン */
+  Widget questionButton(BuildContext context, String questionName, String questionPage){
+    return TextButton(
+      child: Text(
+        questionName,
+        style: TextStyle(
+          fontFamily: 'Kosugi',
+          fontSize: 40,
         ),
-      )
+      ),
+      onPressed: (){
+        //指定した画面に遷移する
+        Navigator.pushNamed(context, questionPage);
+      },
     );
   }
 
@@ -145,37 +73,17 @@ class _MyHomePageState extends State<MyHomePage> {
         child:Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            /* 問題文 */
             Text(
-              "スピッツの曲を選べ（１つは不正解）",
+              "問題選択",
               style: TextStyle(
-                fontFamily: 'Kosugi',
-                fontSize: 40,
-                backgroundColor: Colors.amberAccent,
+                fontSize: 50,
               ),
             ),
-            /* 6つの選択肢を表にして表示する */
-            Table(
-              defaultColumnWidth: FixedColumnWidth(_cardInterval),
-              children:<TableRow>[
-                TableRow(
-                  children: <Widget>[
-                    createChoiceWidget(choices[0]["choice"], choices[0]["answer"]),
-                    createChoiceWidget(choices[1]["choice"], choices[1]["answer"]),
-                    createChoiceWidget(choices[2]["choice"], choices[2]["answer"]),
-                  ]
-                ),
-                TableRow(
-                  children: <Widget>[
-                    createChoiceWidget(choices[3]["choice"], choices[3]["answer"]),
-                    createChoiceWidget(choices[4]["choice"], choices[4]["answer"]),
-                    createChoiceWidget(choices[5]["choice"], choices[5]["answer"]),
-                  ]
-                )
-              ]
-            )
+            /* 問題ページ一覧 */
+            questionButton(context, "問題１", '/1'),
+            questionButton(context, "問題２", '/2'),
           ]
-        )
+        ),
       ),
     );
   }
---
+++ lib/quiz/quizBase.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:flip_card/flip_card.dart';
+
+/**
+ * ドボンクイズの基底クラス
+ */
+class QuizBase extends StatelessWidget {
+
+  // 問題文
+  String question = "";
+
+  // 選択肢カード
+  List<Map<String, String>> choices = [];
+
+  // カードを並べた時の全体の横幅
+  double _cardInterval = 200;
+  // カードの横幅
+  double _cardWidth = 150;
+  // カードの縦幅
+  double _cardHeight = 200;
+
+
+  // 選択肢カードWidget作成
+  Widget createChoiceWidget(String? choice, String? answer){
+    if(choice == null){
+      print("[ERROR] choice is null");
+      choice = 'null';
+    }
+    if(answer == null){
+      print("[ERROR] answer is null");
+      answer = 'true';
+    }
+    return SizedBox(
+      width: _cardWidth,
+      height: _cardHeight,
+      child:Card(
+        elevation: 0.0,
+        margin: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0, bottom: 0.0),
+        color: Color(0x00000000),
+        child: FlipCard(
+          direction: FlipDirection.HORIZONTAL,
+          speed: 1000,
+          onFlipDone: (status) {
+            //todo:効果音追加、ドボン時のエフェクト
+            print(status);
+          },
+          front: Container(
+            decoration: BoxDecoration(
+              color: Color(0x000000),
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  choice,  //選択肢
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                )
+              ],
+            ),
+          ),
+          back: Container(
+            decoration: BoxDecoration(
+              color: Color(0x000000),
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  answer,  //答え
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      )
+    );
+  }
+
+  @override
+  //Widget build(BuildContext context, String question, List<Map<String, String>> choices) {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("問題"),
+      ),
+      body: Center(
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            /* 問題文 */
+            Text(
+              question,
+              style: TextStyle(
+                fontFamily: 'Kosugi',
+                fontSize: 40,
+                backgroundColor: Colors.amberAccent,
+              ),
+            ),
+            /* 6つの選択肢を表にして表示する */
+            Table(
+              defaultColumnWidth: FixedColumnWidth(_cardInterval),
+              children:<TableRow>[
+                TableRow(
+                  children: <Widget>[
+                    createChoiceWidget(choices[0]["choice"], choices[0]["answer"]),
+                    createChoiceWidget(choices[1]["choice"], choices[1]["answer"]),
+                    createChoiceWidget(choices[2]["choice"], choices[2]["answer"]),
+                  ]
+                ),
+                TableRow(
+                  children: <Widget>[
+                    createChoiceWidget(choices[3]["choice"], choices[3]["answer"]),
+                    createChoiceWidget(choices[4]["choice"], choices[4]["answer"]),
+                    createChoiceWidget(choices[5]["choice"], choices[5]["answer"]),
+                  ]
+                )
+              ]
+            ),
+            /* タイトル画面に戻るボタン */
+            TextButton(
+              child: Text("問題選択ページに戻る"),
+              onPressed: (){
+                Navigator.pop(context);
+              },
+            ),
+          ]
+        ),
+      ),
+    );
+  }
+}
